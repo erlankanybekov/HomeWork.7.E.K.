6c675ca9c0bb44cc2996be97c9a8620d273d24e9
erlankanybekov <86570752+erlankanybekov@users.noreply.github.com>
2021-07-25 12:16:52 +0600

    Update Magic.java

--- src/com/company/Magic.java
+++ src/com/company/Magic.java
@@ -11,7 +11,7 @@ public class Magic extends Hero{
 
     @Override
     public String applySuperAbility(String SuperAbilityType) {
-        return "Magic is using Fireball";
+        return "Magic" + SuperAbilityType + "Fireball";
     }
 }
 
